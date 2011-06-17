944269c1db3da5bf2e47fbba592878f075ae6ae9
Vishesh Yadav <vishesh3y@gmail.com>
2011-06-17 10:32:31 +0000

    tag dialog blank
    svn path=/branches/work/soc-kdesdk-dolphin-plugins-hg/dolphin-plugins/; revision=1237200

---
+++ hg/tagdialog.cpp
@@ -0,0 +1,50 @@
+/***************************************************************************
+ *   Copyright (C) 2011 by Vishesh Yadav <vishesh3y@gmail.com>             *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ *   This program is distributed in the hope that it will be useful,       *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+ *   GNU General Public License for more details.                          *
+ *                                                                         *
+ *   You should have received a copy of the GNU General Public License     *
+ *   along with this program; if not, write to the                         *
+ *   Free Software Foundation, Inc.,                                       *
+ *   51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA            *
+ ***************************************************************************/
+
+#include "tagdialog.h"
+#include "hgwrapper.h"
+
+#include <QtCore/QStringList>
+#include <QtGui/QLabel>
+#include <QtGui/QGroupBox>
+#include <QtGui/QFrame>
+#include <QtGui/QHBoxLayout>
+#include <QtGui/QVBoxLayout>
+#include <klocale.h>
+#include <kdebug.h>
+
+HgTagDialog::HgTagDialog(QWidget *parent):
+    KDialog(parent, Qt::Dialog)
+{
+    // dialog properties
+    this->setCaption(i18nc("@title:window", 
+                "<application>Hg</application> Tag"));
+    this->setButtons(KDialog::None);
+
+    // UI 
+    QFrame *frame = new QFrame;
+    QVBoxLayout *vbox = new QVBoxLayout;
+
+    qDebug() << "\n\n\n\n\n\n" 
+        << HgWrapper::instance()->getTags() << "\n\n\n\n\n";
+
+}
+
+#include "tagdialog.moc"
+
