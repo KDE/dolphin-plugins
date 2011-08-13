517ff587300ee0385ac2cf4b33b73ecb53cdcfd4
Vishesh Yadav <vishesh3y@gmail.com>
2011-08-13 10:44:14 +0000

    Added servewrapper and backout skeleton files
    svn path=/branches/work/soc-kdesdk-dolphin-plugins-hg/dolphin-plugins/; revision=1246855

---
+++ hg/backoutdialog.cpp
@@ -0,0 +1,44 @@
+/***************************************************************************
+ *   Copyright (C) 2011 by Vishesh Yadav <vishesh3y@gmail.com>             *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ *   This program is distributed in the hope that it will be useful,       *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+ *   GNU General Public License for more details.                          *
+ *                                                                         *
+ *   You should have received a copy of the GNU General Public License     *
+ *   along with this program; if not, write to the                         *
+ *   Free Software Foundation, Inc.,                                       *
+ *   51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA            *
+ ***************************************************************************/
+
+#include "backoutdialog.h"
+
+#include <klocale.h>
+
+HgBackoutDialog::HgBackoutDialog(QWidget *parent) :
+    KDialog(parent, Qt::Dialog)
+{
+    // dialog properties
+    this->setCaption(i18nc("@title:window", 
+                "<application>Hg</application> Backout"));
+    this->setButtons(KDialog::Ok | KDialog::Cancel);
+    this->setDefaultButton(KDialog::Ok);
+    this->setButtonText(KDialog::Ok, i18nc("@action:button", "Backout"));
+
+    //
+    setupUI();
+}
+
+void HgBackoutDialog::setupUI()
+{
+    //
+}
+
+#include "backoutdialog.moc"
+
---
+++ hg/servewrapper.h
@@ -0,0 +1,39 @@
+/***************************************************************************
+ *   Copyright (C) 2011 by Vishesh Yadav <vishesh3y@gmail.com>             *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ *   This program is distributed in the hope that it will be useful,       *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+ *   GNU General Public License for more details.                          *
+ *                                                                         *
+ *   You should have received a copy of the GNU General Public License     *
+ *   along with this program; if not, write to the                         *
+ *   Free Software Foundation, Inc.,                                       *
+ *   51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA            *
+ ***************************************************************************/
+
+#ifndef HG_SERVE_WRAPPER_H
+#define HG_SERVE_WRAPPER_H
+
+#include <QtCore/QHash>
+#include <QtCore/QProcess>
+#include <QtCore/QString>
+
+class HgServeWrapper
+{
+public: 
+    HgServeWrapper(QWidget *parent=0);
+
+private:
+    static QHash<QString, QProcess> m_serverList;
+};
+
+
+#endif /* HG_SERVE_WRAPPER_H */
+
+
